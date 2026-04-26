90fa1daa58fbc71e0cf451a6043bcb64ac0b1c9a
Guilherme Bigois <guilhermebigois@outlook.com>
2026-04-26 03:36:15 -0300

    chore: add react and jsx-a11y eslint plugins

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,7 @@
 import js from '@eslint/js'
 import importPlugin from 'eslint-plugin-import'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import reactPlugin from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
@@ -11,19 +13,28 @@ export default defineConfig([
         files: ['**/*.{js,jsx}'],
         extends: [
             js.configs.recommended,
+            reactPlugin.configs.flat.recommended,
+            reactPlugin.configs.flat['jsx-runtime'],
             reactHooks.configs.flat.recommended,
             reactRefresh.configs.vite,
+            jsxA11y.flatConfigs.recommended,
             importPlugin.flatConfigs.recommended,
         ],
         languageOptions: {
             globals: globals.browser,
             parserOptions: { ecmaFeatures: { jsx: true } },
         },
+        settings: {
+            react: {
+                version: 'detect'
+            }
+        },
         rules: {
             'no-unused-vars': ['warn', { varsIgnorePattern: '^[A-Z_]' }],
             'quotes': ['error', 'single', { 'avoidEscape': true }],
             'semi': ['error', 'always'],
             'comma-dangle': ['error', 'never'],
+            'react/prop-types': 'off',
             'import/extensions': ['error', 'never', {
                 'png': 'always',
                 'jpg': 'always',
