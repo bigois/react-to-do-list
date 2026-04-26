abe4b57e82fc8940e2322f750b66e7ff20ba0d94
Guilherme Bigois <guilhermebigois@outlook.com>
2026-04-26 00:42:25 -0300

    chore: add comma-dangle to lint

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import importPlugin from 'eslint-plugin-import'
-import {defineConfig, globalIgnores} from 'eslint/config'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
     globalIgnores(['dist', 'node_modules', 'eslint.config.js']),
@@ -17,12 +17,13 @@ export default defineConfig([
         ],
         languageOptions: {
             globals: globals.browser,
-            parserOptions: {ecmaFeatures: {jsx: true}},
+            parserOptions: { ecmaFeatures: { jsx: true } },
         },
         rules: {
-            'no-unused-vars': ['warn', {varsIgnorePattern: '^[A-Z_]'}],
-            'quotes': ['error', 'single', {'avoidEscape': true}],
+            'no-unused-vars': ['warn', { varsIgnorePattern: '^[A-Z_]' }],
+            'quotes': ['error', 'single', { 'avoidEscape': true }],
             'semi': ['error', 'always'],
+            'comma-dangle': ['error', 'never'],
             'import/extensions': ['error', 'never', {
                 'png': 'always',
                 'jpg': 'always',
@@ -31,7 +32,7 @@ export default defineConfig([
                 'svg': 'always',
                 'css': 'always'
             }],
-            'import/no-unresolved': 'off',
+            'import/no-unresolved': 'off'
         }
     }
 ])
